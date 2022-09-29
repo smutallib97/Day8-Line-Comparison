5c45306d6b44210ff695730d5a7833da0adfaaa2
smutallib97 <smutallib97@gmail.com>
2022-09-30 00:46:56 +0530

    Added: Welcome to Line Comparison Computation Program

---
+++ Line_Comparison.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Line_Comparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
+
