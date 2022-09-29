256b235ff2fbd6814380ce8cbdb4e51e36e41e92
smutallib97 <smutallib97@gmail.com>
2022-09-30 00:52:45 +0530

    Added: Calculate length of line

--- Line_Comparison.java
+++ Line_Comparison.java
@@ -1,8 +1,31 @@
 package com.bridgelabz;
-
-public class Line_Comparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+class Point{
+    float x;
+    float y;
+}
+class Line{
+    Point p1;
+    Point p2;
+    float length;
+    float calculateLength(){
+        return (float) Math.sqrt((Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2)));
     }
 }
 
+
+public class Line_Comparison {
+    public static void main(String[] args) {
+        //System.out.println("Welcome to Line Comparison Computation Program");
+        Line line1 = new Line();
+        line1.p1 = new Point();
+        line1.p1.x = 10;
+        line1.p1.y = 8;
+
+        line1.p2 = new Point();
+        line1.p2.x = 20;
+        line1.p2.y = 4;
+
+        float length = line1.calculateLength();
+        System.out.println("Length of the Line is : " +length);
+    }
+}
